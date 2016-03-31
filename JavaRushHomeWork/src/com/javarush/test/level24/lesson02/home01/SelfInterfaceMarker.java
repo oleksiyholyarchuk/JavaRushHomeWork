a62edd7eabdbb5a26a6f30b14de17520f2b277a0
Oleksiy Holyarchuk <oleksiy.holyarchuk@gmail.com>
2016-03-31 20:15:47 +0300

    24th level completed.
    24th level completed with the big one.

---
+++ JavaRushHomeWork/src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by oleksiy.holyarchuk on 31.03.2016.
+ */
+public interface SelfInterfaceMarker
+{
+}
---
+++ JavaRushHomeWork/src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by oleksiy.holyarchuk on 31.03.2016.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker
+{
+    public SelfInterfaceMarkerImpl()
+    {
+    }
+    public void selfie() {}
+    public void laugh() {}
+}
