5c8750ea8d09e0767ea1d36e6e06338e8855ed4f
Oleksiy Holyarchuk <oleksiy.holyarchuk@gmail.com>
2016-04-07 19:20:02 +0300

    27th level completed and The Big One - partially, half-solved.

---
+++ JavaRushHomeWork/src/com/javarush/test/level27/lesson15/big01/ad/NoVideoAvailableException.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level27.lesson15.big01.ad;
+
+/**
+ * Created by oleksiy.holyarchuk on 07.04.2016.
+ */
+public class NoVideoAvailableException extends RuntimeException
+{
+}
